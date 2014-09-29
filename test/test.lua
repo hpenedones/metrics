d628b7a468ac67fdaa9f698a6b7a5365a13f6cd4
Hugo Penedones <hpenedones@gmail.com>
2014-09-29 21:31:52 +0100

    Improved readme. Added image

--- test/test.lua
+++ test/test.lua
@@ -1,29 +1,14 @@
 require 'torch'
 mt = require 'metrics'
-
 gfx = require 'gfx.js'
 
-resp = torch.DoubleTensor(6)
-resp[1] = -0.3
-resp[2] = -0.2
-resp[3] = 0.0
-resp[4] = 0.5
-resp[5] = 0.6
-resp[6] = -0.1
-
-labels = torch.IntTensor(6)
-labels[1] = -1
-labels[2] = 1
-labels[3] = -1
-labels[4] = 1
-labels[5] = 1
-labels[6] = -1
+resp = torch.DoubleTensor { -0.9, -0.8, -0.8, -0.5, -0.1, 0.0, 0.2, 0.2, 0.51, 0.74, 0.89}
+labels = torch.IntTensor  { -1, -1 , 1, -1, -1, 1, 1, -1, -1, 1, 1 }
 
 roc_points = mt.roc.curve(resp, labels)
 area = mt.roc.area(roc_points)
 
 print(roc_points)
-
 print(area)
 
 gfx.chart(roc_points)
